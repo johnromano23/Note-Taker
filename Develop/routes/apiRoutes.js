88d964c99adb80aae966d8b56c04f40eaab54939
John Romano <john_romano23@yahoo.com>
2020-05-25 16:10:56 -0400

    worked on server.js and route connection

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,2 +1,29 @@
+// links the route to the JSON data
 const router = require("express").Router();
 const notes = require("../db/notes.js");
+
+// get notes
+router.get("/notes", (req, res) => {
+  notes
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+// adds items to api
+router.post("/notes", (req, res) => {
+  notes
+    .addNotes(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+// delete items when the user presses the icon
+router.delete("/notes/:id", function (req, res) {
+  notes
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT" + PORT);
+});
