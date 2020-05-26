7d26f5b6d27775bf0291d60847104aa411048ff8
John Romano <john_romano23@yahoo.com>
2020-05-26 18:05:35 -0400

    made changes to server and routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,29 +1,54 @@
 // links the route to the JSON data
 const router = require("express").Router();
-const notes = require("../db/notes.js");
+const fs = require("fs");
 
 // get notes
-router.get("/notes", (req, res) => {
-  notes
-    .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+router.get("/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    res.send(JSON.parse(data));
+  });
+  console.log("GET working");
 });
 
-// adds items to api
-router.post("/notes", (req, res) => {
-  notes
-    .addNotes(req.body)
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+//save notes and append to json file
+router.post("/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+
+    let note = JSON.parse(data);
+    let newNote = req.body;
+    //give an id to each object to retrieve it later
+    newNote.id = uuid();
+    note.push(newNote);
+
+    //write the file
+    fs.writeFile("db/db.json", JSON.stringify(note), (err) => {
+      if (err) throw err;
+    });
+  });
+  res.json("");
 });
 
-// delete items when the user presses the icon
+//retrieve the note onject by id and delete it
 router.delete("/notes/:id", function (req, res) {
-  notes
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    let noteId = req.params.id;
+    let noteData = JSON.parse(data);
+    noteData = noteData.filter(function (note) {
+      if (noteId != note.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    //write file
+    fs.writeFile("db/db.json", JSON.stringify(noteData), function (error) {
+      if (error) throw error;
+      res.send(console.log("Deleted!"));
+    });
+  });
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,18 @@
+// Start with dependencies
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
+// Set up Express
 const app = express();
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+// Server command to begin listening
 app.listen(PORT, function () {
-  console.log("App listening on PORT" + PORT);
+  console.log("Server is listening on: http://localhost" + PORT);
 });
